aec5dda25e26544908cf8c63756d407684acbb92
Sean Reinhardt <seanreinhardtapps@outlook.com>
2015-02-08 23:29:08 -0800

    Added initial SignupViewController

--- SignupViewController.swift
+++ SignupViewController.swift
@@ -10,26 +10,59 @@ import UIKit
 
 class SignupViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var passwordRepeatField: UITextField!
+    
+    @IBOutlet weak var emailField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func signup(sender: AnyObject) {
+        
+        var userName:String = usernameField.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        
+        var password:String = passwordField.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        
+        var passwordRepeat:String = passwordRepeatField.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        
+        var email:String = emailField.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        
+        
+        if userName.utf16Count == 0 || password.utf16Count == 0 || email.utf16Count == 0 || passwordRepeat != password
+        {
+            var alert = UIAlertController(title: "Oops", message: "Please check username, password and email fields.  All field must be filled and password must match", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+        else
+        {
+            var newUser:PFUser = PFUser()
+            newUser.username = userName
+            newUser.password = password
+            newUser.email = email
+            
+            newUser.signUpInBackgroundWithBlock({ (success:Bool, error:NSError!) -> Void in
+                
+                if let err = error
+                {
+                    var alert = UIAlertController(title: "Oops", message: "Error with signup, please retry", preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }
+                else
+                {
+                    self.navigationController?.popToRootViewControllerAnimated(true)
+                }
+            })
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
